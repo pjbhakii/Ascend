171b28a96e708785b461dd5e8511cab33b55f6fb
Prajwal Pawar <37900936+pjbhakii@users.noreply.github.com>
2026-03-08 01:36:54 +0530

    Delete kinetic-state-main (1) directory

--- kinetic-state-main (1)/kinetic-state-main/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ascend.app',
-  appName: 'Ascend ',
-  webDir: 'dist'
-};
-
-export default config;
